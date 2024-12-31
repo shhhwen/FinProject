73a740e35748dd737473ffa4c83ddd078f2f4890
shhhwen <71849923+shhhwen@users.noreply.github.com>
2024-12-31 21:34:50 +0800

    更改商品js

--- assets/js/item.js
+++ assets/js/item.js
@@ -13,3 +13,27 @@ function add(ctn_num) {
     }
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    // 獲取商品 ID
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = parseInt(urlParams.get('id'));
+
+    // 查找商品
+    const product = mockData.find(item => item.id === productId);
+
+    if (product) {
+        // 更新商品信息
+        document.querySelector('.introduce .image img').src = product.image;
+        document.querySelector('.titpro .title_a').innerHTML = `${product.enName}<br>${product.name}`;
+        document.querySelector('.titpro .proprice').innerText = `NT$${product.price}`;
+        document.querySelector('.title .title_c').innerText = `庫存：${product.stock}`;
+        document.querySelector('.description').innerText = product.description;
+        document.querySelector('.ingredients').innerHTML = product.ingredients.map(i => `${i}<br>`).join('');
+    } else {
+        // 商品未找到
+        document.querySelector('main').innerHTML = `<p>商品未找到</p>`;
+    }
+});
+
+// 初始化
+loadProduct();
